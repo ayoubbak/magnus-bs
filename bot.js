945dba3730217b7c158d1ba66dc802972c7f5a3d
AyoubBakkari <39628078+ayoubbak@users.noreply.github.com>
2019-06-25 20:04:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,15 +4,7 @@ const prefix = "%";
 
 
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : MAGNUS ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setStatus("dnd")
-});
+
 
 
 
